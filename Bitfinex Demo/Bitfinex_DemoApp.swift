7783da67cb084b544ad4bf7fdcc832710c534695
Jonathan Gikabu <jonathangikabu@e-kraal.com>
2021-08-28 00:00:56 +0300

    Initial Commit

---
+++ Bitfinex Demo/Bitfinex_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitfinex_DemoApp.swift
+//  Bitfinex Demo
+//
+//  Created by Jonathan Gikabu on 28/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bitfinex_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
